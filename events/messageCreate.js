1c887cc8e7f5b6c3232f96e99bf407e16eb7d17e
Bella Blair <bella_blair@disroot.org>
2025-08-28 17:35:24 +0630

    feat(events): add 'scd' reader
    wip

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,5 +1,15 @@
 const { Events } = require("discord.js");
 const constants = require("../constants.js");
+const {
+  isRegistered,
+  assignReminders,
+  toSeconds,
+  anyRemindersEnabled,
+  hasReminderEnabled,
+} = require("../utils/helpers.js");
+
+const cooldownRegex = /.*\*\*`(Drop|Grab|Series)\s*.*\*\*(.*)\*\*/;
+const timeRegex = /^(?:(?<minutes>\d+)m)?\s*(?:(?<seconds>\d+)s)?$/i;
 
 module.exports = {
   name: Events.MessageCreate,
@@ -20,6 +30,50 @@ module.exports = {
       await command.execute(message, args);
     };
 
-    if (!isRegistered(message.author.id)) return;
+    if (!await isRegistered(message.author.id)) return;
+    // only registered users will pass through here
+
+    if (message.content.split(" ")[0] == "scd") {
+      await sofiCooldownHandler(message);
+    }
   },
 };
+
+async function sofiCooldownHandler(message) {
+  // return if user doesn't have any reminders enabled
+  if (!await anyRemindersEnabled(message.author.id)) return;
+
+  const filter = m =>
+    m.author.id == constants.SOFI &&
+    m.embeds.length > 0 && m.embeds[0].author &&
+      m.embeds[0].author.name.includes(message.author.username);
+  const collector = message.channel.createMessageCollector({ filter, time: 5000 });
+
+  collector.on("collect", async (m) => {
+    const desc = m.embeds[0].description;
+    for (const line of desc.split("\n")) {
+      const match = cooldownRegex.exec(line);
+      if (!match) continue;
+
+      const reminderName = match[1].toLowerCase();
+      if (!await hasReminderEnabled(message.author.id, reminderName)) continue;
+
+      const reminder = match[2];
+      const timeMatch = timeRegex.exec(reminder);
+      if (!timeMatch) {
+        await assignReminders(message.author.id, reminderName, "0");
+      }
+      else {
+        const minutes = timeMatch.groups.minutes || "0";
+        const seconds = timeMatch.groups.seconds || "0";
+        const reminderTime = Math.ceil(+Date.now() / 1000) + toSeconds(minutes, seconds);
+        await assignReminders(message.author.id, reminderName, reminderTime.toString());
+      }
+
+    }
+  });
+
+  collector.on("end", collected => {
+    if (!collected.size) return;
+  });
+}
