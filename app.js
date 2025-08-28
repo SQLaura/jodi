0e7b348b0798d0634d4739c17a2238265a8a20eb
Bella Blair <bella_blair@disroot.org>
2025-08-28 15:42:21 +0630

    refactor: change the way events and commands are loaded
    it should be more flexible now

--- app.js
+++ app.js
@@ -1,56 +1,59 @@
-const { Client, Events, Collection, GatewayIntentBits } = require("discord.js");
+const { Client, ActivityType, Events, Collection, GatewayIntentBits } = require("discord.js");
 const fs = require("node:fs");
 const client = new Client(
   {
     intents: [
-      GatewayIntentBits.MessageContent,
+      GatewayIntentBits.Guilds,
       GatewayIntentBits.GuildMessages,
+      GatewayIntentBits.MessageContent,
     ],
   },
 );
 
 client.commands = new Collection();
 
-console.log("=".repeat(process.stdout.columns || 16));
-
-fs.readdir("./events/", (err, files) => {
-  if (err) return console.error;
-  let i = 0;
-  files.forEach(file => {
-    if (!file.endsWith(".js")) return;
-
-    const event = require(`./events/${file}`);
-    const eventName = file.split(".")[0];
-    console.log(`Loaded event: '${eventName}'`);
-    client.on(eventName, event.bind(null, client));
-    i++;
-  });
-  console.log();
-  console.log(`Loaded ${i} events successfully`);
+function divider() {
   console.log();
   console.log("=".repeat(process.stdout.columns || 16));
-});
-
-fs.readdir("./commands/", (err, files) => {
-  let i = 0;
-  files.forEach(file => {
-    if (!file.endsWith(".js")) return;
-
-    const command = require(`./commands/${file}`);
-    const commandName = file.split(".")[0];
-    console.log(`Loaded command: '${commandName}'`);
-    client.commands.set(commandName, command);
-    i++;
-  });
   console.log();
-  console.log(`Loaded ${i} commands successfully`);
-  console.log();
-  console.log("=".repeat(process.stdout.columns || 16));
-});
+}
 
+divider();
+
+const events = fs.readdirSync("./events/").filter(file => file.endsWith("js"));
+let i = 0;
+for (const eventFile of events) {
+  const event = require(`./events/${eventFile}`);
+  const eventName = eventFile.split(".")[0];
+  console.log(`Loaded event: '${eventName}'`);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  }
+  else {
+    client.on(event.name, (...args) => event.execute(...args));
+  }
+  i++;
+}
+
+console.log(`Loaded ${i} events successfully`);
+divider();
+
+i = 0;
+
+const commands = fs.readdirSync("./commands/").filter(file => file.endsWith("js"));
+for (const commandFile of commands) {
+  const command = require(`./commands/${commandFile}`);
+  const commandName = command.name;
+  console.log(`Loaded command: '${commandName}'`);
+  client.commands.set(commandName, command);
+}
+
+console.log(`Loaded ${i} commands successfully`);
+divider();
 
 client.once(Events.ClientReady, bot => {
   console.log(`Logged in as ${bot.user.username}`);
+  client.user.setActivity("a barbershop haircut that cost a quarter", { type: ActivityType.Custom });
 });
 
 client.login(process.env.BOT_TOKEN);
