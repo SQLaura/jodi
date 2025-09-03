21682a4062e5af44d1a3b481f1f56885c2ff939c
Bella Blair <bella_blair@disroot.org>
2025-09-04 00:35:26 +0630

    feat(reminders): add text at the end of drop/grab reminders
    drop: checks for grab and if no grab is ready, it will show next grab
    grab: checks for drop and if no drop is ready, it will link #dungeon-1

--- utils/reminder_helpers.js
+++ utils/reminder_helpers.js
@@ -1,4 +1,5 @@
-const { TextChannel } = require("discord.js");
+// eslint-disable-next-line no-unused-vars
+const { TextChannel, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
 const prisma = require("../prismaConnection");
 const {
   assignReminders,
@@ -39,9 +40,32 @@ async function checkReminders(client) {
         const channel = await client.channels.fetch(userData.channel);
         await assignReminders(userID, reminder, "0");
         userData[reminder] = "0";
+        let text = `<@${userID}> your **\`${reminder}\`** is ready!`;
+        const actionRow = new ActionRowBuilder();
+
+        if (reminder === "drop" && userData["grab"] !== "0") {
+          text += `\n-# your grab will be ready <t:${userData["grab"]}:R>`;
+        }
+        else if (reminder === "grab" && userData["drop"] !== "0") {
+          text += `\n-# your drop will be ready <t:${userData["drop"]}:R>, you can click on the button to jump to #dungeon-1 of official server`;
+          const button = new ButtonBuilder()
+            .setLabel("Jump")
+            .setURL("https://discord.com/channels/863226120245215262/929778705469943829")
+            .setStyle(ButtonStyle.Link);
+          actionRow.addComponents(button);
+        }
 
         setTimeout(() => {
-          channel.send(`<@${userID}> your **\`${reminder}\`** is ready!`);
+          if (actionRow.components.length === 0) {
+            channel.send(text);
+          }
+          else {
+            channel.send({
+              content: text,
+              // @ts-ignore
+              components: [actionRow],
+            });
+          }
         }, (reminderTime - currentTime) * 1000);
       }
     }
